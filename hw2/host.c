18d4f8f748d8603aa2d220d07f6dc4c7a00910b1
b03902048 <b03902048@csie.ntu.edu.tw>
2015-11-22 17:32:19 +0800

    modify host.c and player.c, now fifo communication is done

--- hw2/host.c
+++ hw2/host.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <time.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <sys/types.h>
@@ -17,49 +19,95 @@ int main(int argc, char *argv[]) {
     }
     int host_id = atoi(argv[1]);
 
-    int fifo_ptoh, fifo_htop[4];
+    int fifo_htop[4];
+    FILE* fifo_ptoh;
     char fifoname[5][20];
     setUpFifoName(fifoname, host_id);
 
-    int num[4];
     char in_buf[1024];
     memset(in_buf, 0, sizeof(char) * 1024);
+
+    srand((unsigned int) time(NULL));
+    char playerIndex[] = {'A', 'B', 'C', 'D'};
+    char params[3][10];
+    pid_t childPID;
+    int playerID[4];
+    int playerHash[4];
+    int playerMoney[4];
+    int playerScore[4];
+
+    char cmpt_msg[30];
+    char rtn_msg[30];
+    char rtn_index;
+    int rtn_hash, rtn_money;
+    int msg_len;
+
     while (1) {
         read(STDIN_FILENO, in_buf, 1024);
-        sscanf(in_buf, "%d %d %d %d\n", &num[0], &num[1], &num[2], &num[3]);
+        sscanf(in_buf, "%d %d %d %d\n", &playerID[0], &playerID[1], &playerID[2], &playerID[3]);
 
-        if (num[0] == -1 && num[1] == -1 && num[2] == -1 && num[3] == -1)
+        if (playerID[0] == -1 && playerID[1] == -1 && playerID[2] == -1 && playerID[3] == -1)
             break;
+        
+        for (int i = 0; i < 4; i++) {
+            // fork child process now
+            playerHash[i] = rand() % 65536;
+            playerMoney[i] = playerScore[i] = 0;
 
-        printf("read: %d %d %d %d\n", num[0], num[1], num[2], num[3]);
-    }
-/*
-    pid_t child;
-    child = fork();
+            childPID = fork();
+            if (childPID < 0) {
+                perror("fork() error");
+                exit(-1);
+            }
 
-    if (child < 0) {
-        perror("fork() error");
-        exit(-1);
-    }
+            if (childPID == 0) {
+                memset(params[0], 0, sizeof(char) * 10);
+                memset(params[1], 0, sizeof(char) * 10);
+                memset(params[2], 0, sizeof(char) * 10);
+                sprintf(params[0], "%d", host_id);
+                sprintf(params[1], "%c", playerIndex[i]);
+                sprintf(params[2], "%d", playerHash[i]);
+                execlp("./player", "./player", params[0], params[1], params[2], (char*)NULL);
+                _exit(EXIT_FAILURE);
+            }
+            else {
+                fifo_htop[i] = open(fifoname[i+1], O_WRONLY | O_TRUNC);
+            }
+        }
 
-    if (child != 0) {
-        printf("I'm parent %d, and my child is %d\n", getpid(), child);
-    } else {
-        printf("I'm child\n");
-        execlp("sleep", "sleep", "3", (char*)NULL);
-        exit(0);
-    }
-*/
-    int status;
-    pid_t wpid;
-    while (1) {
-        wpid = wait(&status);
-        if (wpid == -1 && errno == ECHILD)
-            break;
-    }
+        truncate(fifoname[0], 0);
+        fifo_ptoh = fopen(fifoname[0], "r");
+        // fifo_ptoh = open(fifoname[0], O_RDONLY | O_TRUNC);
 
-    for (int i = 0; i < 5; i++)
-        unlink(fifoname[i]);
+        for (int i = 0; i < 1; i++) {
+            for (int j = 0; j < 4; j++)
+                playerMoney[j] += 1000;
+
+            memset(cmpt_msg, 0, sizeof(char) * 30);
+            sprintf(cmpt_msg, "%d %d %d %d\n", playerMoney[0], playerMoney[1], playerMoney[2], playerMoney[3]);
+            msg_len = strlen(cmpt_msg);
+
+            for (int j = 0; j < 4; j++) {
+                write(fifo_htop[j], cmpt_msg, msg_len);
+            }
+
+            memset(rtn_msg, 0, sizeof(char) * 30);
+            while (fgets(rtn_msg, 30, fifo_ptoh) != NULL) {
+                sscanf(rtn_msg, " %c %d %d\n", &rtn_index, &rtn_hash, &rtn_money);
+                fprintf(stderr, "from player%c : hash = %d, money = %d\n", rtn_index, rtn_hash, rtn_money);
+                memset(rtn_msg, 0, sizeof(char) * 30);
+            }
+        }
+
+        // after all thing done, close something
+        int status;
+        pid_t wpid;
+        while ((wpid = wait(&status)) > 0);
+        fclose(fifo_ptoh);
+        for (int i = 0; i < 4; i++) close(fifo_htop[i]);
+    } //  END of while loop
+
+    for (int i = 0; i < 5; i++) unlink(fifoname[i]);
 
     return 0;
 }
--- hw2/player.c
+++ hw2/player.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
 
 int main(int argc, char *argv[]) {
     
-    if (argc > 1) {
-        for (int i = 1; i < argc; i++) {
-            puts(argv[i]);
-        }
+    if (argc != 4) {
+        fprintf(stderr, "usage: ./player [host_id] [player_index] [random_key]\n");
+        exit(-1);
     }
+
+    char i_fifoname[20];
+    char o_fifoname[20];
+    sprintf(i_fifoname, "host%s_%s.FIFO", argv[1], argv[2]);
+    sprintf(o_fifoname, "host%s.FIFO", argv[1]);
+
+    char buf[128], w_buf[128];
+    int bid[4];
+    memset(buf, 0, sizeof(char) * 128);
+    int fifo_htop = open(i_fifoname, O_RDONLY);
+    int fifo_ptoh = open(o_fifoname, O_WRONLY | O_APPEND);
+
+    read(fifo_htop, buf, 128);
+
+    sscanf(buf, "%d %d %d %d\n", &bid[0], &bid[1], &bid[2], &bid[3]);
+    memset(w_buf, 0, sizeof(char) * 128);
+    sprintf(w_buf, "%s %s 1000\n", argv[2], argv[3]);
+    write(fifo_ptoh, w_buf, strlen(w_buf));
+
+    close(fifo_htop);
+    close(fifo_ptoh);
 
     return 0;
 }
